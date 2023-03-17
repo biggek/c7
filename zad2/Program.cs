ee52b7378769f812a2c2f28a4d5bcb48bbd63aba
Sergey <lolito0700@mail.ru>
2023-03-17 17:15:40 +0300

    zad2

---
+++ zad2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите кол-во столбцов массива:");
+int col = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите кол-во строк массива:");
+int row = int.Parse(Console.ReadLine());
+double[,] randomArray = new double[col,row];
+
+Console.WriteLine("Введите позицию столбца:");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите позицию строки: ");
+int n = int.Parse(Console.ReadLine());
+
+void mas(int col, int row)
+    {
+        int i,j;
+        Random rand = new Random();
+        for (i = 0; i < col; i++)
+            {
+                Console.WriteLine();
+                for (j = 0; j < row; j++)
+                {
+                    randomArray[i,j] = rand.NextDouble();
+                    Console.WriteLine($"{randomArray[i,j]:F2}");
+                }
+                     Console.WriteLine();
+            }   
+    }
+mas(col,row);
+if (m<1 || n<1)
+Console.WriteLine("Позиции строк не могут быть отрицательными");
+else if (m <= col+1 && n <= row+1)
+Console.WriteLine($"Значение элемента равно {randomArray[m-1,n-1]:F2}");
+else Console.WriteLine("Такого элемента нет в массиве");
