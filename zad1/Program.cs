e5e285603dc410653c70ef85db48196e00600edc
Sergey <lolito0700@mail.ru>
2023-03-17 16:49:26 +0300

    zad1

---
+++ zad1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите кол-во строк массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов  массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] randomArray = new double[m,n];
+
+void mas(int m, int n)
+{
+int i,j;
+Random rand = new Random();
+for (i = 0; i < m; i++)
+{   
+    Console.WriteLine();
+    for (j = 0; j < n; j++)
+{
+    randomArray[i,j] = rand.NextDouble();
+    Console.WriteLine($"{randomArray[i,j]:F2}");
+}
+
+}
+
+}
+
+mas(m,n);
