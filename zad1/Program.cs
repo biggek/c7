4116a76275ae2c8d6dcc2cf28342c50b4fda26c6
Sergey <lolito0700@mail.ru>
2023-03-17 16:56:14 +0300

    zad1.2

--- zad1/Program.cs
+++ zad1/Program.cs
@@ -5,20 +5,18 @@ int col = Convert.ToInt32(Console.ReadLine());
 double[,] randomArray = new double[row,col];
 
 void mas(int row, int col)
-{
-int i,j;
-Random rand = new Random();
-for (i = 0; i < row; i++)
-{   
-    Console.WriteLine();
-    for (j = 0; j < col; j++)
-{
-    randomArray[i,j] = rand.NextDouble();
-    Console.WriteLine($"{randomArray[i,j]:F2}");
-}
+    {
+    int i,j;
+    Random rand = new Random();
+        for (i = 0; i < row; i++)
+        {   
+            Console.WriteLine();
+            for (j = 0; j < col; j++)
+                 {
+                         randomArray[i,j] = rand.NextDouble();
+                        Console.WriteLine($"{randomArray[i,j]:F2}");
+                }
+        }
 
-}
-
-}
-
+    }
 mas(row,col);
